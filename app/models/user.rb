49eaef2498e25d255d1a031ea8d4d20e3767f353
Renato Panda <renato.panda@ipt.pt>
2017-12-05 22:21:37 +0000

    add user and role views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 	has_many :user_roles
 	has_many :roles, through: :user_roles
 
+	validates_presence_of :email, :password
+	validates_confirmation_of :email, :password
 
 	def role?(role)
 		roles.any? { |r| r.name.underscore.to_sym == role }
